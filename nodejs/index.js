d6b2d44cac8a0fe52bc4a7d70d9f0bd667d14a7d
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2019-12-11 15:21:45 -0400

    Added nodejs to the learning project with some packages

---
+++ nodejs/index.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+const supervillains = require('supervillains');
+
+console.log(superheroes.random());
+console.log(supervillains.random());
+
