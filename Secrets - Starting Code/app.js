280ffd5feb3a5c3fd671ab9e10d22d309c1acd2f
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2019-12-20 15:46:09 -0400

    added packages for Passport authentication

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const encrypt = require("mongoose-encryption");
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const app = express();
@@ -19,7 +20,7 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true
 });
 
-userSchema = new mongoose.Schema({
+userSchema = mongoose.Schema({
     email: {
         type: String,
         required: true
@@ -32,13 +33,7 @@ userSchema = new mongoose.Schema({
 
 console.log(process.env.SECRET);
 
-// const secret = "Thisisalittlesecret.";
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ["password"]
-});
-
-const User = new mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 app.listen(3000, function () {
     console.log("Running");
@@ -55,18 +50,21 @@ app.route("/login").get(function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({
-        email: username
-    }, function (err, found) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (found) {
-                if (found.password === password) {
-                    res.render("secrets");
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+
+        User.findOne({
+            email: username
+        }, function (err, found) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (found) {
+                    bcrypt.compare(password, hash, function (err, resp) {
+                        res.render("secrets");
+                    });
                 }
             }
-        }
+        });
     });
 });
 
@@ -74,16 +72,20 @@ app.route("/register").get(function (req, res) {
     res.render("register");
 
 }).post(function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
